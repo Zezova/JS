106b258aee883f407f10b929480860748a3a0fff
Katja Zezova <zezovakatja@gmail.com>
2023-03-15 23:19:17 +0300

    Update HW_JS_1.js

--- HW_JS_1.js
+++ HW_JS_1.js
@@ -25,19 +25,15 @@ let age_1 = 10
 let age_2 = 18
 let age_3 = 60
 
-if (age_1 < age_2) {
-console.log("You don't have access cause your age is " + age_1 + "It's less than " + age_2)
+if(age_1 < age_2) {
+    console.log("You don't have access cause your age is " + age_1 + " It's less than " + age_2)
 }
-
-else if (age_3 > age_1 >= age_2) {
-console.log("Welcome!")
+else if(age_3 > age_1 >= age_2) {
+    console.log("Welcome!")
 }
-
-else if (age_1 > age_3) {
-console.log("Keep calm and watch Culture channel")
+else if(age_1 > age_3) {
+    console.log("Keep calm and look Culture channel")
 }
-
 else {
-console.log("Technical work")
+    console.log("Technical work")
 }
-
